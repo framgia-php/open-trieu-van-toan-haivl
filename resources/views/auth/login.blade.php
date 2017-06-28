06ff71b38dceb1d5076f5419e016243c1177f4bf
trieutoan <trieutoan195@gmail.com>
2017-06-28 23:18:17 +0700

    login with facebook

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,6 @@
                     <form class="form-horizontal" role="form" method="POST" 
                     action="{{ url('admin/login') }}">
                         {{ csrf_field() }}
-
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
